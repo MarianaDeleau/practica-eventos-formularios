a52e0b653b03b0203c6326211369251a4f999afe
Mariana Deleau <marianadeleau@gmail.com>
2021-06-21 14:58:51 -0300

    exercise image 1 and 2 improved

--- eventos.js
+++ eventos.js
@@ -231,11 +231,15 @@
 // Inicialmente los ítems deben estar en negrita y con color negro. Cuando se clickea uno de ellos, se debe sacarle la negrita y ponerle un color gris (simulando que el email fue leído)
 
 // const mail = document.getElementsByTagName('li');
+
+
 // const readed = (event) => {
 //     const items = event.target
 //     items.classList.add('readed');
 //     items.classList.remove('negrita');
 // }; 
+
+
 // for(let i = 0; i < mail.length; i++) {
 //     mail[i].addEventListener('click', readed)
 // }
@@ -260,7 +264,7 @@
 // const divHijo = document.getElementById('IdHijo');
 // const buttons = document.getElementsByClassName('btn-counter');
 // const progress = document.getElementById('progreso');
-// let widthHijo = '0';
+// let widthHijo = "0";
 // divHijo.style.width= `${widthHijo}%`;
 
 
@@ -302,7 +306,7 @@
 
 // document.onkeyup = progresoFlechas
 
-// //MUESTRA EL VALOR DEL INPUT SLIDER. 
+//MUESTRA EL VALOR DEL INPUT SLIDER. 
 
 // const slider = document.getElementById("slider");
 // const output = document.getElementById("demo");
@@ -376,17 +380,19 @@
 // Crear un documento html con al menos 4 imágenes chicas (100px de alto) y una imagen grande (500px de alto). Cuando se clickean las imágenes chicas, la imagen grande se tiene que actualizar con la misma que la imagen chica clickeada.
 
 
-// const chica = document.getElementsByClassName('chica');
-// const grande = document.getElementsByClassName('grande')[0];
-// const cambio = (event) => {
-//         const fotoChica=event.target;
-//         const srcChica = fotoChica.getAttribute('src');
-//         grande.setAttribute('src' , srcChica)
-// }
-// for(let i = 0; i < chica.length; i++) {
+const chica = document.getElementsByClassName('chica');
+const grande = document.getElementsByClassName('grande')[0];
+const cambio = (event) => {
+        const fotoChica=event.target;
+        const srcChica = fotoChica.getAttribute('src');
+        const srcGrande = grande.getAttribute('src')
+        grande.setAttribute('src' , srcChica)
+        fotoChica.setAttribute('src', srcGrande)
+}
+for(let i = 0; i < chica.length; i++) {
 
-//  chica[i].addEventListener('click', cambio)
-// }
+ chica[i].addEventListener('click', cambio)
+}
 
 
 //-------------------------------------------------
@@ -445,6 +451,39 @@
 // si el número seleccionado es igual al número aleatorio generado, debe mostrar Adivinaste.
 // El mensaje también debe incluir el número ingresado (por ejemplo, Número ingresado: 3). El mensaje debe mostrarse en un elemento del documento html, no en un alert.
 
+// const inputNumero= document.getElementById("input");
+// const adivinar=document.getElementById("adivinar-numero")
+// const numeroAleatorio= (parseInt(Math.random()*9));
+// let numeroIngresado;
+
+// inputNumero.addEventListener('change', () => {
+
+// numeroIngresado=parseInt(inputNumero.value);
+// //console.log(numeroIngresado);
+
+// if(numeroIngresado>numeroAleatorio) {
+//     adivinar.innerText=`el número es más chico. Número ingresado: ${numeroIngresado}`;
+//     //alert(`el número es más chico. Número ingresado: ${numeroIngresado}`);
+// } else if(numeroIngresado<numeroAleatorio){
+//     adivinar.innerText = `el número es más grande. Número ingresado: ${numeroIngresado}`;
+//     //alert(`el número es más grande. Número ingresado: ${numeroIngresado}`);
+// } else if(numeroIngresado===numeroAleatorio){
+//     adivinar.innerText =`Adivinaste!. Número ingresado: ${numeroIngresado}`;
+//     //alert(`Adivinaste!. Número ingresado: ${numeroIngresado}`);
+// };
+
+// })
+
+//const ingrese=prompt('Ingrese un número');
+//const numeroIngresado=parseInt(ingrese);
+
+//console.log(numeroAleatorio)
+
+
+
+
+
+
 // Por ejemplo:
 
 // Número aleatorio: 7 (No se muestra)
@@ -457,362 +496,16 @@
 // El documento también debe tener un botón que diga Reiniciar, que genere un nuevo número y actualice el mensaje para indicarlo (por ejemplo ¡Nueva partida! Presione un número.)
 
 
-//-------------------------------------EVENTOS DE FORMULARIO------------------------------------------------
+// const progresoFlechas = (event) => {
 
-// Kilómetros a Millas
-// Crear una página que:
+//     if(event.keyCode == '39'){
+//         widthHijo = parseInt(widthHijo)+10;
+//         divHijo.style.width= `${widthHijo}%`;
+//         progress.innerText=`${widthHijo}%`  
+//     } else if (event.keyCode == '37') {
+//         widthHijo = parseInt(widthHijo)-10;
+//         divHijo.style.width= `${widthHijo}%`;
+//         progress.innerText=`${widthHijo}%`  
+//     }
 
-// Tenga dos inputs, uno para el valor de kilómetros y otro para el de millas.
-// Cuando se modifica alguno de los dos inputs, el otro cambie automáticamente, realizando la conversión adecuada. Tener en cuenta que ki´lómetro es 0.62 millas, y una milla es 1.61 kilómetros.
-
-// const calculo=(event)=>{
-//   let kilometros=document.getElementById('kilometros').value;
-//   let millas=document.getElementById('millas').value;
-
-//   if(event.target.name==='kilometros'){
-
-//     console.log(kilometros*1.61);
-
-//   }  else if (event.target.name==='millas'){
-
-//     console.log(millas*0.62);
-
-//   } 
-// };
-
-// kilometros.addEventListener('change',calculo);
-// millas.addEventListener('change',calculo);
-
-
-
-//---------------opcion MARIANA
-
-// const calculo=(event)=>{
-//   let kilometros=document.getElementById('kilometros').value;
-//   let millas=document.getElementById('millas').value;
-
-//   if(event.target.name==='kilometros'){
-//     document.getElementById('millas').value=kilometros*1.61;
-
-//   }  else if (event.target.name==='millas'){
-//     document.getElementById('kilometros').value=millas*0.62;
-
-//   } 
-// };
-
-// kilometros.addEventListener('change',calculo);
-// millas.addEventListener('change',calculo);
-
-// -------------opcion ADRIAN
-
-// let kilometros=document.getElementById('kilometros');
-// let millas=document.getElementById('millas');
-
-// const calculo=(event)=>{
-//     if(event.target.name==='kilometros') {
-//    millas.value = kilometros.value * 1.61;
-
-//   }  else if (event.target.name==='millas') {
-//     kilometros.value = millas.value * 0.62;
-
-//   } 
-// };
-
-// kilometros.addEventListener('keyup',calculo);
-// millas.addEventListener('keyup',calculo);
-
-
-//-----------------------------------------------------------------------
-
-
-// RGB
-// Crear una página que:
-
-// Tenga tres inputs numéricos, con valor mínimo 0 y valor máximo 255, uno para el valor R (red), otro para el valor G (green) y otro para el valor B (blue).
-// Todos los inputs comiencen con el valor 255.
-// Cuando se modifica algún input, se cambie el color de fondo del body con el color que se forma a partir de dichos valores.
-
-// ------------------VERSION CON FUNCION
-
-// const redInput = document.getElementById('red');
-// let red = redInput.value;
-// const greenInput = document.getElementById('green');
-// let green = greenInput.value;
-// const blueInput = document.getElementById('blue');
-// let blue = blueInput.value;
-// const fondo = document.querySelector('body');
-// let colorRgb=`rgb(${red}, ${green}, ${blue})`;
-// fondo.style.backgroundColor=colorRgb;
-
-// const cambioColor = (r, g, b) => {
-
-//     fondo.style.backgroundColor=`rgb(${r}, ${g}, ${b})`;
 // }
-
-// redInput.addEventListener('input', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//     cambioColor(red, green, blue);//     
-// });
-
-// greenInput.addEventListener('input', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//    cambioColor(red, green, blue);
-// });
-
-// blueInput.addEventListener('input', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//     cambioColor(red, green, blue);
-// });
-
-// -------------VERSION SIN FUNCION
-
-
-// const redInput = document.getElementById('red');
-// let red = redInput.value;
-// const greenInput = document.getElementById('green');
-// let green = greenInput.value;
-// const blueInput = document.getElementById('blue');
-// let blue = blueInput.value;
-// const fondo = document.querySelector('body');
-// let colorRgb=`rgb(${red}, ${green}, ${blue})`;
-// fondo.style.backgroundColor=colorRgb;
-
-
-// redInput.addEventListener('keyup', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//     let colorRgb=`rgb(${red}, ${green}, ${blue})`;
-//     fondo.style.backgroundColor=colorRgb;
-// });
-
-// greenInput.addEventListener('keyup', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//     let colorRgb=`rgb(${red}, ${green}, ${blue})`;
-//     fondo.style.backgroundColor=colorRgb;
-    
-// });
-
-// blueInput.addEventListener('keyup', () => {
-//     red=redInput.value;
-//     green=greenInput.value;
-//     blue=blueInput.value;
-//     let colorRgb=`rgb(${red}, ${green}, ${blue})`;
-//     fondo.style.backgroundColor=colorRgb;
-// });
-
-
-
-
-//---------------------------codigo RESET PAGINA CECI
-// function reloadPage(){
-//     window.location.reload();
-// }
-// reset.onclick= reloadPage; //Para resetear la página con el "onclick"
-
-//-----------------------------------------------------------------------
-
-// Todo
-// Crear una página que:
-
-// Tenga un input, un botón que diga Crear todo y una lista.
-// Al presionar el botón, si el input no está vacío, se agregue un ítem a la lista con el contenido del input y se borre el contenido del input.
-
-
-
-// Agregar imágenes
-// Crear una página que:
-
-// Tenga un input para la url de la imagen, un botón que diga Agregar imagen y un contenedor flexible donde mostrar imágenes como en una grilla.
-// Al presionar el botón, si el input no está vacío, se agregue una imagen al contenedor cuyo src es el contenido del input y se borre el contenido del input.
-// Al hacer click en una imagen, esta se elimine.
-
-
-
-// Comentario
-// Crear una página que:
-
-// Tenga un textarea, un texto pequeño debajo de este y un botón que diga Enviar.
-// El texto comience diciendo 0/240 caracteres.
-// Cuando se escribe algo en el textarea, se actualice el texto para reflejar la cantidad de caracteres restantes.
-// Al presionar el botón, si hay 240 caracteres o menos, se borre el contenido del input y se actualice el texto para que diga Comentario enviado.
-// El color del borde del textarea cambie de la siguiente forma:
-// normal si está vacío
-// verde si tiene texto y 240 caracteres o menos
-// rojo si tiene más de 240 caracters
-
-
-
-// Conversor avanzado
-// Crear una página que:
-
-// Tenga
-// un input para ingresar el valor a convertir
-// un select para seleccionar la unidad del valor convertido
-// un select para seleccionar la unidad a convertir
-// un botón que diga Intercambiar
-// un texto que muestre el resultado
-// Cuyos selects tengan las siguientes opciones:
-// Kilómetros (km)
-// Metros (m)
-// Decímetros (dm)
-// Centímetros (cm)
-// Milímetros (mm)
-// Ambos select comiencen con la opción metros seleccionada y el input comience con el valor 1.
-// Al seleccionar una nueva opción en cualquiera de los select o al modificar el valor del input, se actualice el texto con el resultado de la conversión del valor ingresado de una unidad a la otra.
-// Al clickear el botón Intercambiar se intercambien las opciones de los selects y se actualice el texto.
-
-
-
-
-// TIP
-
-// Para realizar este ejercicio, el value de cada option del select deben ser los siguiente:
-
-// Para Kilómetros, el value debe ser 1000
-// Para Metros, el value debe ser 1
-// Para Decímetros, el value debe ser 0.1
-// Para Centímetros, el value debe ser 0.01
-// Para Milímetros, el value debe ser 0.001
-// La conversión se realiza con la fórmula: VALOR_INGRESADO * VALOR_UNIDAD / VALOR_UNIDAD_A_CONVERTIR.
-
-
-
-
-// Card dinámica
-// Crear una página que:
-
-// Tenga un input para la url de la imagen, un input para el título, un textarea para la descripción, un input para la url externo y un checkbok que diga Link externo
-// Tenga una card simple y estilizada con: imagen, título, descripción, y un link que diga Ver más.
-// A medida que se modifiquen los campos, se modifique respectivamente los distintos elementos de la card.
-// Si el checkbox no está seleccionado, el link Ver más no se muestre y el input de la url externa se deshabilite.
-// Si el checkbox está seleccionado, el link Ver más se muestre y el input de la url externa se habilite.
-
-
-
-// Selector de imágenes
-// Crear una página que:
-
-// Tenga un select con categorías de imágenes (por ejemplo, animales, comida, paisajes, etc) y un contenedor con al menos 9 imágenes correspondientes a las distintas categorías, mostradas como grilla.
-// El select tenga la opción Todas y comience con dicha opción seleccionada.
-// Al seleccionar una nueva opción en el select se oculten aquellas imágenes que no pertecen a la categoría seleccionada y se muestren las que sí.
-// Al seleccionar Todas se muestren todas las imágenes.
-// TIP: Agregar a cada imagen un atributo data-categoria cuyo valor se corresponda con el atributo value del option de la categoría correspondiente.
-
-// Buscador
-// Crear una página que:
-
-// Tenga un input y un contenedor con al menos 9 imágenes mostradas como grilla y un texto para mostrar la cantidad de resultados encontrados.
-// Al escribir algo en el input aquellas imágenes cuyo alt contiene parte o todo el texto ingresado se muestran, mientras las demás se ocultan.
-// La búsqueda ignora mayúsculas y minúsculas.
-// Al vaciar el input, se muestran todas las imágenes.
-// Al realizar la búsqueda se actualiza el texto con la cantidad de imágenes que se están mostrando, de modo que diga, por ejemplo, 3 resultados encontrados.
-
-// Por ejemplo, si una imagen tiene un alt que dice Oso koala comiendo eucaliptus y se busca ko dicha imagen debe mostrarse.
-
-
-
-
-// Filtro de imágenes
-// Crear una página que:
-
-// Tenga una grilla de imágenes y al menos 4 checkbox con categorías (por ejemplo, animales, comida, paisajes, etc).
-// Al seleccionar un checkbox se actualice la grilla, mostrando aquellas imágenes que pertenezcan a las categorías seleecionadas.
-// Al deseleccionar todos los checkbox se muestren todas las imágenes
-// TIP: Agregar a cada imagen un atributo data-categoria cuyo valor se corresponda con el atributo value del checkbox de la categoría correspondiente.
-
-
-
-// Feedback (Validación)
-// Debemos crear una encuesta de satisfacción al cliente para un portal de ventas basado en 4 preguntas que nos determinará qué tan contento está el cliente con nuestro servicio. Las respuestas deben ser radio buttons y cada grupo responderá a una pregunta específica.
-
-// ¿Qué tan rápido solucionamos tu problema?
-
-// Demoraron mucho
-// Lo recibí en el tiempo estimado
-// Super rápido
-// ¿Como recibiste el producto?
-
-// Tuvo un desperfecto
-// Era tal cual la publicación
-// Superó mis expectativas
-// ¿Cómo calificarías el asesoramiento de nuestros representantes?
-
-// No supo responder adecuadamente mis preguntas
-// El representante respondió adecuadamente
-// Recibí un excelente asesoramiento
-// ¿Recomendarías nuestro portal a otro usuario?
-
-// No lo creo
-// Solo para casos puntuales
-// ¡Si, por supuesto!
-// Una vez que obtengamos las respuestas mostraremos con una alerta el nivel de satisfacción con nuestro servicio, siendo que:
-
-// Las respuestas negativas tendrán un valor de 1
-// Las respuestas neutras un valor de 2
-// Mientras que las respuestas positivas un valor de 3.
-// Por lo que el resultado deberá corresponder al siguiente para el puntaje total:
-
-// 4: Sumamente insatisfecho
-// 5-6: Insatisfecho
-// 6-8: Conforme
-// 8-10: Satisfecho
-// 11-12: Muy satisfecho
-
-
-
-
-// Tarjeta de crédito (Validación)
-// Realizaremos una validación sobre el formulario del ejercicio Pago.
-
-// Aplicaremos las siguientes validaciones:
-
-// Paso 1: Campo del número de la tarjeta de crédito.
-
-// El campo debe tener 16 caracteres. Al salirse del foco de este input, y en el caso de no cumplir con este requerimiento, el input debe colorearse de rojo, le mostraremos un mensaje de error que la tarjeta debe contener 16 caracteres y borraremos el contenido el input.
-// Adicionaremos un input de tipo selector con 2 opciones. Visa y Mastercard. Esto no debe estar disponible para la edición del usuario, pero aplicaremos un cambio dada la siguiente condición: Si el número ingresado por el usuario inicia con 5. Será Mastercard, si comienza con 3 o 4 será visa. Y si inicia con otro valor mostraremos un mensaje de error indicando el número ingresado es erróneo.
-// Paso 2: Campo nombre.
-
-// Validar que el campo de nombre posea solo texto. Lo validaremos con una regex.
-// Paso 3: CCV.
-
-// Validar que solo posea solo números
-// Que su contenido sea 3 caracteres en el caso de visa y 4 en el caso de mastercard. Por lo que habilitaremos esta opción una vez haya completado correctamente el campo numérico.
-
-// En el Paso 1: Usaremos blur y change
-
-// Mientras que las validaciones del Paso 2 y Paso 3 las haremos antes de intentar enviar el formulario.
-
-
-
-
-// Registro (Validación)
-// Antes de enviar el formulario de registro de cliente creado con anterioridad, realizaremos algunas validaciones sobre este. Pero primero, agregaremos una etiqueta small debajo de cada input con un color rojo y de inicio no va a visualizarse.
-
-// Los errores y las validaciones a mostrar deben ser las siguientes:
-
-// Nombre: "El campo nombre solo puede contener texto"
-// Apellido: "El campo apellido solo puede contener texto"
-// DNI: "El DNI no puede ser mayor a 8 caracteres"
-// Email: "El formato del email no es válido"
-// Contraseña: En este caso utilizaremos 2. "La contraseña debe tener al menos 8, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter especial" y "Las contraseñas no coinciden"
-// Términos y condiciones de uso: "Debe aceptar los términos y condiciones"
-// En el caso de que alguno de los inputs no cumpla con las características solicitadas, debemos visualizar el error correspondiente a ese input.
-
-// Usar las siguientes funciones para validar los datos con expresiones regulares:
-
-// const esEmailValido = (email) => {
-//   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//   return regex.test(email.toLowerCase())
-// }
-
-// const esPasswordValida = (password) => {
-// //   const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
